1a275d50a7153c25c8f4882e71cb3b5cca0ad5a7
brewerj3 <joshuabrewer784@gmail.com>
2022-12-12 09:14:02 -1000

    reformatted

--- height.c
+++ height.c
@@ -8,38 +8,34 @@ struct treeNode {
 };
 
 
-struct treeNode * createNode(int data)
-{
+struct treeNode *createNode(int data) {
     struct treeNode *p = (struct treeNode *) malloc(sizeof(struct treeNode));
-    p->data=data;
-    p->left=NULL;
-    p->right=NULL;
+    p->data = data;
+    p->left = NULL;
+    p->right = NULL;
     return p;
 }
 
-void deleteTree(struct treeNode *t)
-{
-    if (t==NULL) return;
+void deleteTree(struct treeNode *t) {
+    if (t == NULL) return;
     deleteTree(t->left);
     deleteTree(t->right);
     free(t);
 }
 
-struct treeNode * tree1()
-{
-    struct treeNode * root = createNode(5);
+struct treeNode *tree1() {
+    struct treeNode *root = createNode(5);
     root->left = createNode(3);
     root->left->left = createNode(0);
-    root->right =createNode(8);
+    root->right = createNode(8);
     root->right->left = createNode(6);
     root->right->left->right = createNode(7);
     root->right->right = createNode(9);
     return root;
 }
 
-struct treeNode * tree2()
-{
-    struct treeNode * root = createNode(5);
+struct treeNode *tree2() {
+    struct treeNode *root = createNode(5);
     root->left = createNode(3);
     root->right = createNode(8);
     root->right->left = createNode(6);
@@ -47,18 +43,15 @@ struct treeNode * tree2()
     return root;
 }
 
-int height(struct treeNode *t)
-{
+int height(struct treeNode *t) {
     return 0;
 }
 
-struct treeNode * tree3()
-{
+struct treeNode *tree3() {
     return createNode(5);
 }
 
-int main()
-{
+int main() {
     struct treeNode *t = tree1();
     printf("Tree 1: height=%d\n", height(t));
     deleteTree(t);
--- topsort.c
+++ topsort.c
@@ -6,102 +6,93 @@
 
 struct node {
     int id;
-    struct node * next;
+    struct node *next;
 };
 
-struct node ** createAdjList(int n) /* Create adjacency list of of n nodes */
+struct node **createAdjList(int n) /* Create adjacency list of of n nodes */
 {
-    struct node ** adjList = (struct node **) malloc(sizeof(struct node *)*n);
-    for (int i=0; i<n; i++) {
+    struct node **adjList = (struct node **) malloc(sizeof(struct node *) * n);
+    for (int i = 0; i < n; i++) {
         adjList[i] = NULL;
     }
     return adjList;
 }
 
-void destroyAdjList(struct node ** adjList, int n)
-{
-    struct node * p;
-    struct node * temp;
-    for (int i=0; i<n; i++) {
-        for (p=adjList[i]; p!=NULL;) {
-            temp=p;
-            p=p->next;
+void destroyAdjList(struct node **adjList, int n) {
+    struct node *p;
+    struct node *temp;
+    for (int i = 0; i < n; i++) {
+        for (p = adjList[i]; p != NULL;) {
+            temp = p;
+            p = p->next;
             free(temp);
         }
     }
     free(adjList);
 }
 
-struct node * createNode(int n)
-{
+struct node *createNode(int n) {
     struct node *i = (struct node *) malloc(sizeof(struct node));
-    i->id=n;
-    i->next=NULL;
+    i->id = n;
+    i->next = NULL;
     return i;
 }
 
-struct node ** insertLink(int i, int j, struct node ** adjList)
-{
-    struct node * temp = adjList[i];
+struct node **insertLink(int i, int j, struct node **adjList) {
+    struct node *temp = adjList[i];
     adjList[i] = createNode(j);
     adjList[i]->next = temp;
     return adjList;
 }
 
-struct node ** createGraph1()
-{
-    struct node ** graph1= createAdjList(NUMNODES);
-    graph1 = insertLink(1,2,graph1);
-    graph1 = insertLink(1,3,graph1);
-    graph1 = insertLink(3,2,graph1);
-    graph1 = insertLink(3,0,graph1);
-    graph1 = insertLink(2,5,graph1);
-    graph1 = insertLink(2,0,graph1);
-    graph1 = insertLink(0,5,graph1);
-    graph1 = insertLink(0,4,graph1);
-    graph1 = insertLink(5,4,graph1);
+struct node **createGraph1() {
+    struct node **graph1 = createAdjList(NUMNODES);
+    graph1 = insertLink(1, 2, graph1);
+    graph1 = insertLink(1, 3, graph1);
+    graph1 = insertLink(3, 2, graph1);
+    graph1 = insertLink(3, 0, graph1);
+    graph1 = insertLink(2, 5, graph1);
+    graph1 = insertLink(2, 0, graph1);
+    graph1 = insertLink(0, 5, graph1);
+    graph1 = insertLink(0, 4, graph1);
+    graph1 = insertLink(5, 4, graph1);
     return graph1;
 }
 
-void displayList(struct node * list)
-{
-    for (struct node * p=list; p!=NULL; p=p->next) {
-        printf("->%2d ",p->id);
+void displayList(struct node *list) {
+    for (struct node *p = list; p != NULL; p = p->next) {
+        printf("->%2d ", p->id);
     }
     printf("\n");
 }
 
 
-
-struct node * topsort(struct node ** adjList, int n)
-{
+struct node *topsort(struct node **adjList, int n) {
     return NULL;
 }
 
 
-void displayAdjList(struct node ** adjList, int n)
-{
-    struct node * p;
-    for (int i=0; i<n; i++) {
-        printf("%2d:",i);
-        for (p=adjList[i]; p!= NULL; p=p->next) {
-            printf("->%2d",p->id);
+void displayAdjList(struct node **adjList, int n) {
+    struct node *p;
+    for (int i = 0; i < n; i++) {
+        printf("%2d:", i);
+        for (p = adjList[i]; p != NULL; p = p->next) {
+            printf("->%2d", p->id);
         }
         printf("\n");
     }
     printf("\n");
 }
 
-int main()
-{
-    struct node ** graph1 = createGraph1();
+int main() {
+    struct node **graph1 = createGraph1();
     printf("Adjacency list of graph 1\n");
     displayAdjList(graph1, NUMNODES);
 
     printf("Topologically sorted list ");
-    struct node * list = topsort(graph1, NUMNODES);
+    struct node *list = topsort(graph1, NUMNODES);
     displayList(list);
 
-    destroyAdjList(graph1,NUMNODES);
+    destroyAdjList(graph1, NUMNODES);
 }
 
